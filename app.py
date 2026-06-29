343ce1f59e193f10a99f744379599904c50a35d2
shivam1108-06 <shivamkotak118@gmail.com>
2026-06-29 13:39:16 +0530

    Added flashcard question answer feature

--- app.py
+++ app.py
@@ -3,23 +3,37 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 decks = []
+flashcards = []
 
 @app.route("/", methods=["GET", "POST"])
 def home():
 
     if request.method == "POST":
 
-        deck_name = request.form["deck_name"]
-        category = request.form["category"]
+        if "deck_name" in request.form:
 
-        decks.append({
-            "deck_name": deck_name,
-            "category": category
-        })
+            deck_name = request.form["deck_name"]
+            category = request.form["category"]
+
+            decks.append({
+                "deck_name": deck_name,
+                "category": category
+            })
+
+        elif "question" in request.form:
+
+            question = request.form["question"]
+            answer = request.form["answer"]
+
+            flashcards.append({
+                "question": question,
+                "answer": answer
+            })
 
     return render_template(
         "index.html",
-        decks=decks
+        decks=decks,
+        flashcards=flashcards
     )
 
 if __name__ == "__main__":
