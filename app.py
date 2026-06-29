46fbb49d5bd2255fd2c33dfca66179e4bb997761
shivam1108-06 <shivamkotak118@gmail.com>
2026-06-29 13:20:00 +0530

    Added create deck functionality

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+decks = []
+
+@app.route("/", methods=["GET", "POST"])
 def home():
 
+    if request.method == "POST":
+
+        deck_name = request.form["deck_name"]
+        category = request.form["category"]
+
+        decks.append({
+            "deck_name": deck_name,
+            "category": category
+        })
+
     return render_template(
-        "index.html"
+        "index.html",
+        decks=decks
     )
 
 if __name__ == "__main__":
